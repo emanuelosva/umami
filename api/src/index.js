0df31b4a84302e44b6bb697ad74bd2bbae74c9f5
emanuelosva <emanuelosva@gmail.com>
2020-06-18 21:41:13 -0500

    Server configuration

--- api/src/index.js
+++ api/src/index.js
@@ -4,3 +4,23 @@
  * @version 1.0
  * @author Emanuel Osorio <emanuelosva@gamil.com>
 */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('../config');
+
+const PORT = config.port;
+const app = express();
+
+// App extensiones
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Router
+
+// Expose
+app.listen(PORT, () => {
+  console.log(`Api run on http://localhost:${PORT}`);
+});
