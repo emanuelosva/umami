f2f6753b4024e590b9a585d226a1617ca42d9364
emanuelosva <emanuelosva@gmail.com>
2020-06-20 12:38:14 -0500

    db manage

---
+++ api/src/components/recipe/model.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview Model schema DB for recipe.
+ *
+ * @version 1.0
+ * @author Emanuel Osorio <emanuelosva@gmail.com>
+ *
+*/
+
+const mongoose = require('mongoose'); s
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema({
+  id: {
+    type: Schema.ObjectId,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  tag: {
+    type: String,
+    required: true,
+  },
+  servings: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  ingredients: [{
+    type: Object,
+    required: true,
+  }],
+  instructions: [{
+    type: String,
+    required: true,
+  }],
+  description: {
+    type: String,
+    required: true,
+  },
+  url_img: {
+    type: String,
+    required: true,
+  },
+});
+
+const model = mongoose.model('Recipe', recipeSchema, 'recipes');
+module.exports = model;
---
+++ api/src/components/recipe/store.js
@@ -0,0 +1,52 @@
+/**
+ * @fileoverview DB store manage for recipes.
+ *
+ * @version 1.0
+ * @author Emanuel Osorio <emanuelosva@gmail.com>
+ *
+*/
+
+const Model = require('./model');
+
+const list = async () => {
+  const data = await Model.find();
+  return data;
+};
+
+const get = async (id) => {
+  const data = await Model.findById(id)
+  return data;
+};
+
+const add = async (data) => {
+  const newRecipe = new Model(data);
+  return newRecipe.save();
+};
+
+const update = async (id, data) => {
+  const recipe = await Model.findById(id);
+
+  recipe.name = data.name;
+  resipe.tag = data.tag;
+  recipe.servings = data.servings;
+  recipe.time = data.time;
+  recipe.ingredients = data.ingredients;
+  recipe.instructions = data.instructions;
+  recipe.description = data.description;
+  recipe.url_img = data.url_img;
+
+  const updatedRecipe = await recipe.save()
+  return updatedRecipe;
+};
+
+const remove = async (id) => {
+  return Model.deleteOne({ _id: id });
+};
+
+module.exports = {
+  list,
+  get,
+  add,
+  update,
+  remove,
+};
