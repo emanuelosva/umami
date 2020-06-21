2724bdadc3f61bad489d91217f8e23c2117bf75c
emanuelosva <emanuelosva@gmail.com>
2020-06-20 19:00:33 -0500

    Change a dynamic store

---
+++ api/src/store.js
@@ -0,0 +1,92 @@
+/**
+ * @fileoverview DB store manage.
+ *
+ * @version 1.0
+ * @author Emanuel Osorio <emanuelosva@gmail.com>
+ *
+*/
+
+function store(injectedModel) {
+
+  const Model = injectedModel
+
+
+  const list = async () => {
+    try {
+      const data = await Model.find();
+      return data;
+    } catch (err) {
+      config.enviroment.development
+        ? console.log(`[store] -> Error: ${err}`)
+        : null;
+
+      return Promise.reject('Not found');
+    }
+  };
+
+  const get = async (id) => {
+    try {
+      const data = await Model.findById(id);
+      return data;
+    } catch (err) {
+      config.enviroment.development
+        ? console.log(`[store] -> Error: ${err}`)
+        : null;
+
+      return Promise.reject('Not found');
+    }
+  };
+
+  const add = async (data) => {
+    try {
+      const newAdmin = new Model(data);
+      return newAdmin.save();
+    } catch (err) {
+      config.enviroment.development
+        ? console.log(`[store] -> Error: ${err}`)
+        : null;
+
+      return Promise.reject('Internal error');
+    }
+  };
+
+  const update = async (id, data) => {
+    try {
+      const admin = await Model.findById(id);
+
+      admin.user = data.user;
+      admin.password = data.password;
+
+      const updatedAdmin = await admin.save();
+      return updatedAdmin;
+    } catch (err) {
+      config.enviroment.development
+        ? console.log(`[store] -> Error: ${err}`)
+        : null;
+
+      return Promise.reject('Internal error');
+    }
+  };
+
+  const remove = async (id) => {
+    try {
+      return Model.deleteOne({ _id: id });
+    } catch (err) {
+      config.enviroment.development
+        ? console.log(`[store] -> Error: ${err}`)
+        : null;
+
+      return Promise.reject('Internal error');
+    }
+  };
+
+  return {
+    list,
+    get,
+    add,
+    update,
+    remove
+  };
+};
+
+module.exports = store;
