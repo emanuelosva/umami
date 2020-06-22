dc6b56e05edd1d6933835e8b15bf9a7b2b59255b
emanuelosva <emanuelosva@gmail.com>
2020-06-22 11:44:00 -0500

    db model for users

---
+++ api/src/components/user/model.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Model schema DB for recipe.
+ *
+ * @version 1.0
+ * @author Emanuel Osorio <emanuelosva@gmail.com>
+ *
+*/
+
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  plan: {
+    type: String,
+    required: true,
+  },
+});
+
+const _model = model('User', userSchema, 'users');
+module.exports = _model;
