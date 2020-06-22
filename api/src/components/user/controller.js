8de8987f8d9289f116bc505a24d69ec5bba2eda0
emanuelosva <emanuelosva@gmail.com>
2020-06-22 11:58:07 -0500

    Bussiness logic for users added

---
+++ api/src/components/user/controller.js
@@ -0,0 +1,69 @@
+/**
+ * @fileoverview Bussines Logic for users
+ *
+ * @version 1.0
+ * @author Emanuel Osorio <emanuelosva@gmail.com>
+ *
+*/
+
+const storeManage = require('../../store');
+const Model = require('./model');
+
+const store = storeManage(Model);
+
+const list = async (filter) => {
+  let user;
+  filter
+    ? user = await store.get(filter)
+    : user = await store.list()
+
+  return recipe;
+};
+
+const add = async (body) => {
+
+  if (!body.name || !body.email || !body.password) {
+    return Promise.reject('Invalid data');
+  }
+
+  let user = {
+    name: body.name,
+    email: body.email,
+    password: body.password,
+    created: new Date(),
+    updated: new Date(),
+  };
+
+  const newUser = await store.add(user);
+  return newUser;
+};
+
+const update = (id, body) => {
+  if (!id || !body) {
+    return Promise.reject('No id or data')
+  }
+
+  let user = {
+    name: body.name,
+    email: body.email,
+    password: body.password,
+    updated: new Date(),
+  };
+
+  return store.update(id, user)
+};
+
+const remove = (id) => {
+  if (!id) {
+    return Promise.reject('No id');
+  }
+
+  return store.remove(id);
+};
+
+module.exports = {
+  list,
+  add,
+  update,
+  remove,
+};
