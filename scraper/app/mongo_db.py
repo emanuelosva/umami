6e6148ed450d0db5d06102a72fd1600e0b7d6b30
Esteban Monguí <esteband.itorres@gmail.com>
2020-06-24 13:46:23 -0500

    cree función add description en mongo_db.py

--- scraper/app/mongo_db.py
+++ scraper/app/mongo_db.py
@@ -6,3 +6,8 @@ CONECTION_STRING = "mongodb+srv://<username>:<password>@cluster0-cialg.mongodb.n
 client = pymongo.MongoClient(CONECTION_STRING)
 db = client.get_database('db_flaskuser_umami')
 recipes_collection = "recipes" 
+
+
+def add_description(recipie_description) -> str:
+    recipe_ref = db.collection(recipes_collection).document(recipe_id)
+    recipe_ref.add({'description':description})
