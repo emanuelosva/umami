69354e91654c8e725530b2b27bd334c2c6aab949
Esteban Monguí <esteband.itorres@gmail.com>
2020-06-25 10:59:50 -0500

    Creando estructura de funciones para añadir elementos de receta a la base de datos

--- scraper/app/mongo_db.py
+++ scraper/app/mongo_db.py
@@ -11,3 +11,21 @@ db = client.get_database('db_flaskuser_umami')
 def add_description(description):
     recipe_ref = db.collection("recipes").document(recipe_id)
     recipe_ref.add({'description':description})
+
+def add_ingredients(ingredients):
+    pass
+
+def add_instructions(instructions):
+    pass
+
+def add_name(name):
+    pass
+
+def add_servings(servings):
+    pass
+
+def add_time(time):
+    pass
+
+def add_url_img(url_image):
+    pass
