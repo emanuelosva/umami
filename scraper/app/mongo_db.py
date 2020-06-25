56de110956740770a864a5f4029e59d15c0ed47c
Esteban Monguí <esteband.itorres@gmail.com>
2020-06-25 14:22:29 -0500

    creando función insert_recipe y eliminando estructura inicial

--- scraper/app/mongo_db.py
+++ scraper/app/mongo_db.py
@@ -1,26 +1,6 @@
-from flask import Blueprint
 from .extensions import mongo
 
-mongo_db = Blueprint('mongo_db', __name__)
 
-def add_description(description):
-    recipe_ref = mongo.db.collection("recipes").document(recipe_id)
-    recipe_ref.add({'description':description})
-
-def add_ingredients(ingredients):
-    pass
-
-def add_instructions(instructions):
-    pass
-
-def add_name(name):
-    pass
-
-def add_servings(servings):
-    pass
-
-def add_time(time):
-    pass
-
-def add_url_img(url_images):
-    pass
+def insert_recipe(recipe):
+    recipe = mongo.db.collection("recipes")
+    recipe.insert({'recipe' : recipe})
